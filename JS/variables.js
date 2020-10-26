a3f17ae8ca3cbb7f77d9c63ca141eb84c1e03359
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2020-10-26 16:15:08 +0530

    variables

---
+++ JS/variables.js
@@ -0,0 +1,21 @@
+// "use strict"  // can't declare variable directly.
+// default mode -- sloppy
+
+var a=10;
+b=20;
+
+console.log(c)
+
+function fuc()
+{
+    d=30; // globally
+    let e=40; // blockscope
+    var f=50;
+    // window.g=60;  //can be access in browser 
+}
+
+console.log(a)
+console.log(d)
+console.log(f)
+
+const z=50;
