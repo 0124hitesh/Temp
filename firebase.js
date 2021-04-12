0b05df733be390c2f7ab705e02dddbb8a7ba7e9c
0124hitesh <hitesh.sharma_cs18@gla.ac.in>
2021-04-12 22:32:08 +0530

    ..

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyARYQRwukPu0nZ-qyJE6nZQKspMVtHhLKA",
+    authDomain: "skind-e9c97.firebaseapp.com",
+    databaseURL: "https://skind-e9c97.firebaseio.com",
+    projectId: "skind-e9c97",
+    storageBucket: "skind-e9c97.appspot.com",
+    messagingSenderId: "117274580030",
+    appId: "1:117274580030:android:d61852b087028dee15ed6f",
+    measurementId: "G-MEASUREMENT_ID",
+  };
